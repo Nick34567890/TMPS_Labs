81e0c0092728f1c074cd123332ab338ae989e376
Nick <nichita.gancear@isa.utm.md>
2025-10-22 15:25:10 +0300

    refactoring

--- Labs/com/grocery/storage/Storage.java
+++
@@ -1,9 +0,0 @@
-package grocery.storage;
-
-import grocery.models.Item;
-import java.util.List;
-
-public interface Storage {
-    List<Item> loadItems();           // method 1
-    void saveItems(List<Item> items); // method 2
-}
